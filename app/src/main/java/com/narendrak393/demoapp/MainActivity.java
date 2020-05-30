5825239016da1fadd8b8f6cf7fe605c4ad9cfc1a
Narendra Kumar <narendrak393@gmail.com>
2020-05-30 19:00:46 +0530

    initial project

--- app/src/main/java/com/narendrak393/demoapp/MainActivity.java
+++ app/src/main/java/com/narendrak393/demoapp/MainActivity.java
@@ -1,17 +1,22 @@
 package com.narendrak393.demoapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    public Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getApplicationContext();
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.text_box);
         textView.setText(R.string.admin);
-        textView.setTextColor(getApplicationContext().getColor(R.color.colorAccent));
-        textView.setBackgroundColor(getApplicationContext().getColor(R.color.colorPrimary));
+        textView.setTextColor(mContext.getColor(R.color.colorAccent));
+        textView.setBackgroundColor(mContext.getColor(R.color.colorPrimary));
     }
 }
