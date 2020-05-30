3730096fd2c1860227665db0d2f02352196e78df
Narendra Kumar <narendrak393@gmail.com>
2020-05-30 18:12:50 +0530

    initial project

--- app/src/main/java/com/narendrak393/demoapp/MainActivity.java
+++ app/src/main/java/com/narendrak393/demoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.narendrak393.demoapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text_box);
+        textView.setText(R.string.admin);
     }
 }
